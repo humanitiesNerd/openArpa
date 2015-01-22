6ad270b06e1be6aef3d8f070a3ffcb19ba153644
humanitiesNerd <catonano@gmail.com>
2015-01-22 12:39:09 +0100

    ora stampa le date correttamente

--- openArpa.py
+++ openArpa.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
 import xlrd
+import datetime
 import csv
 from os import sys
+
+def cell_value(cell, datemode):
+    if cell.ctype == xlrd.XL_CELL_DATE:
+        if cell.value != 0.0:
+            t = xlrd.xldate_as_tuple(cell.value, datemode)
+            if t[0] > 0:
+                val = datetime.datetime(*t)
+            else:
+                val = datetime.time(*t[3:])
+        else:
+            val = EPOCH
+    else:
+        val = cell.value
+        if isinstance(val, float):
+            if int(val) == val:
+                # int masquerading as a float?
+                val = int(val)
+    return val
+
+#cell_value(a_cell, workbook.datemode)
+
 
 def csv_from_excel(excel_file):
     workbook = xlrd.open_workbook(excel_file)
@@ -12,8 +34,14 @@ def csv_from_excel(excel_file):
         wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
 
         for rownum in xrange(worksheet.nrows):
-            wr.writerow([unicode(entry).encode("utf-8") for entry in worksheet.row_values(rownum)])
+            row = []
+            for colnum in xrange(worksheet.ncols):
+                row.append(worksheet.cell(rownum, colnum))
+            s2 = [unicode(cell_value(cell, workbook.datemode)).encode("utf-8") for cell in row]
+     
+            wr.writerow(s2)
         your_csv_file.close()
+
 
 if __name__ == "__main__":
     csv_from_excel(sys.argv[1])
