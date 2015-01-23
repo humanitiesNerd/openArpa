bca9c9f4410e53b1146d853990558d25af92a5e2
humanitiesNerd <catonano@gmail.com>
2015-01-23 01:20:10 +0100

    a bit of tidy up

--- src/open_arpa/core.clj
+++ src/open_arpa/core.clj
@@ -2,8 +2,6 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(comment {"NMHC" "NOx" "PRESS." "RAD.GLOB." "DV" "RADSG." "VVP" "H2S" "RADST" "Benzene" "V.V." "CH4" "UMID" "NO2" "VV" "RADS" "D.V." "PM2.5" "SIGMA" "TEMP" "RADSG" "PRESS" "NOX" "Pioggia" "RADGLOB" "TEMP." "O3" "PM10 B" "PRESSIONE" "UMID." "RADSN" "NO" "Toluene" "DVG" "SO2" "IPA TOT" "TEMP EST" "E-BENZENE" "BENZENE" "PM10" "UMR" "UMR." "CO" "NOxapi" "PM10 biora" "PIOGGIA" "TOLUENE" "UMIDITA E" "DVP" "O-XYLENE" "MP-XYLENE" "NO2api"}
-)
 
 (def adige (io/file "resources/files/adige.csv"))
 (def talsano (io/file "resources/files/talsano.csv"))
@@ -21,18 +19,6 @@
 (defn fifth-rows [files]
   (map select-the-nth-row-in-a-csv-file files (repeat 5)))
 
-(defn unique-columns [third-rows-collection]
-"goes through all the files, select the third row in each file and creates a set of all the unique acronyms used there. Such set is gonna be be the set of columns in the final order. With the set of files of 2013 this is the output
-
-{\"NMHC" "NOx" "PRESS." "RAD.GLOB." "DV" "RADSG." "VVP" "H2S" "RADST" "Benzene" "V.V." "CH4" "UMID" "NO2" "VV" "RADS" "D.V." "PM2.5" "SIGMA" "TEMP" "RADSG" "PRESS" "NOX" "Pioggia" "RADGLOB" "TEMP." "O3" "PM10 B" "PRESSIONE" "UMID." "RADSN" "NO" "Toluene" "DVG" "SO2" "IPA TOT" "TEMP EST" "E-BENZENE" "BENZENE" "PM10" "UMR" "UMR." "CO" "NOxapi" "PM10 biora" "PIOGGIA" "TOLUENE" "UMIDITA E" "DVP" "O-XYLENE" "MP-XYLENE" "NO2api}\""
-(cons "Parm" (into () (set (mapcat next third-rows-collection)))))
-
-
-(defn new-order [columns-set]
-  (let [final-indices (range (count columns-set))]
-    (sort-by :value  (map (fn [column index]
-                            {:value column :index-new index})
-                          columns-set final-indices))))
 
 (defn file-order [file-contents]
   (let [columns (select-the-nth-row-in-a-csv-file file-contents 3)
@@ -41,20 +27,6 @@
     (sort-by :index (map (fn [index column measurement-unit]
                            {:substance column :measurement-unit measurement-unit :index index})
                          indices columns measure-units ))))
-
-
-
-(defn file-reordering-structure [new-order order-per-file]
-  (sort-by :index-old <
-           (map first (filter (fn [sigh] (not (empty? sigh)) )
-                              (map (fn [new-thing]
-                                     (filter
-                                      (fn [thing] (not (nil? thing)))
-                                      (map (fn [old-thing] (if (= (:value new-thing) (:value old-thing))
-                                                             (conj new-thing old-thing)))
-                                           order-per-file)))
-                                   new-order)))))
-
 
 (defn file-as-csv [file]
   (list  (.getName file) (csv/read-csv (io/reader file))))
