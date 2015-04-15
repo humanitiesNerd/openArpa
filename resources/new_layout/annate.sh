406f52698dd48a4f7cc0b5891b27d1ff3c778ba9
humanitiesNerd <catonano@gmail.com>
2015-04-15 12:53:56 +0200

    added some data

---
+++ resources/new_layout/annate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir 2005
+mkdir 2006
+mkdir 2007
+mkdir 2008
+mkdir 2009
+mkdir 2010
+mkdir 2011
+mkdir 2012
+mkdir 2013
---
+++ resources/new_layout/centraline.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+mkdir Altamura
+mkdir Caldarola
+mkdir Kennedy
+mkdir Japigia
+mkdir Cavour
+mkdir Stanic
+mkdir Politecnico
+mkdir Carbonara
+mkdir CUS
+mkdir Casamassima
+mkdir ENAIP
+mkdir Verdi
+mkdir ASM
+mkdir Monopoli
+mkdir "Monopoli ItalGreen"
+mkdir Casale
+mkdir "Via dei Mille"
+mkdir SISRI
+mkdir "Via Taranto"
+mkdir "IT Nautico Carnaro"
+mkdir "Terminal Passeggeri"
+mkdir "Via Cappuccini"
+mkdir Perrino
+mkdir "Ceglie Messapica"
+mkdir "Via Fabio Filzi"
+mkdir Mesagne
+mkdir "San Pancrazio Salentino"
+mkdir "San Pietro Vernotico"
+mkdir Valzani
+mkdir "San Vito dei Normanni"
+mkdir Torchiarolo
+mkdir "Torchiarolo- Via Fanin"
+mkdir Lendinuso
+mkdir "Via Vaccina"
+mkdir "Via Casardi"
+mkdir Rosati
+mkdir "Capitaneria di Porto"
+mkdir "via dei Mandorli"
+mkdir "Suolo Giuffreda"
+mkdir "Arnesano Riesci"
+mkdir "Campi Salentina"
+mkdir "Santa Barbara"
+mkdir "ITC La Porta"
+mkdir Guagnano
+mkdir "S. Maria Cerrate"
+mkdir "P.zza Libertini"
+mkdir "Via Garigliano"
+mkdir "Piazza Palio"
+mkdir "ITC De Castro"
+mkdir Giorgiloro
+mkdir "Via B. Croce"
+mkdir Grottaglie
+mkdir "Martina Franca"
+mkdir "Via Frappietri"
+mkdir Statte
+mkdir "SS7 Ponte Wind"
+mkdir Talsano
+mkdir "Via Archimede"
+mkdir "San Vito"
+mkdir "Via Alto Adige"
+mkdir "Via Machiavelli"
+mkdir "Paolo VI - CISI"
+mkdir "Via Antonucci"
