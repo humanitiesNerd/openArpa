3b5ad7b8d728acaa54a5298cd83dc3fc42c3a949
humanitiesNerd <catonano@gmail.com>
2015-07-14 09:16:36 +0200

    I required the core namespace from inside the user namespace, So I spare myself a require in the REPL at every session

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,7 @@
 (ns user
   (:require [reloaded.repl :refer [system init start stop go reset]]
-            [open-arpa.systems :refer [dev-system]]))
+            [open-arpa.systems :refer [dev-system]]
+            [open-arpa.core :as op]))
 
 (reloaded.repl/set-init! dev-system)
+
