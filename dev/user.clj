21bb0e8c190592e0f8b4969aa235f1a7f2b930b6
humanitiesNerd <catonano@gmail.com>
2015-07-22 01:03:47 +0200

    using the steps in the user namespace to lead the development

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,17 @@
 (ns user
   (:require [reloaded.repl :refer [system init start stop go reset]]
             [open-arpa.systems :refer [dev-system]]
-            [open-arpa.core :as op]))
+            [open-arpa.core :as op]
+            [open-arpa.dictionaries :refer [pollutants]]))
 
 (reloaded.repl/set-init! dev-system)
 
+(def step1 (op/ingested-file op/Altamura-test))
+
+(def step2 (op/splitted-file step1))
+
+(def step3 (op/added-file-order step2))
+
+;;(def process (map op/parse-dates handle))
+
+;; (dorun (println process))
